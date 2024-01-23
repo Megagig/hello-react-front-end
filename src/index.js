888b7d61ed3728e88791d53b87c6a81c84c0ca31
[Megagig] <[megagigsoftwaresolution@gmail.com]>
2024-01-23 13:14:46 +0100

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
+import store from './redux/store'; // Add a trailing comma here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
